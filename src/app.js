c46ae8c3ece9e64250730a5e4c9eebdb817ad7b0
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-04-10 12:36:13 -0300

    Add route to delete a repository

--- src/app.js
+++ src/app.js
@@ -45,8 +45,20 @@ app.put('/repositories/:id', (request, response) => {
   return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id
+  );
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
