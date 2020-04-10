c8300bf172598e99226af28672f8181b9706202a
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-04-10 12:34:56 -0300

    Add route to edit a repository

--- src/app.js
+++ src/app.js
@@ -24,8 +24,25 @@ app.post('/repositories', (request, response) => {
   return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id
+  );
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  const likes = repositories[repositoryIndex].likes;
+
+  repository = { id, title, url, techs, likes };
+  
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
